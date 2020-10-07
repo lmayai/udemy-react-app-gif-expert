0728e686dc8b2b4181a21c423948b1b1453df20f
Leonardo <leonardomayairegui@gmail.com>
2020-10-07 15:26:29 -0500

    gif expert app

---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['Naruto']);
+
+  // const handleAdd = () => {
+  //   //setCategories([...categories,'Naruto'])
+  //   setCategories(cats => [...cats,'Naruto'])
+  // }
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+      <AddCategory setCategories={setCategories}/>
+      <hr/>
+      
+      <ol>
+        {
+          categories.map( (category) => 
+            <GifGrid
+              key= {category}
+              category={ category }
+            />
+          )
+        }
+      </ol>
+
+    </>
+  )
+}
+
+
+export default GifExpertApp;
